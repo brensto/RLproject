4ec2e758d60673211dfa003991690d7b6857e7d1
brensto <40505208+brensto@users.noreply.github.com>
2022-08-13 10:34:53 -0500

    v0.8.0.1
    +Lightning Scrolls now appear in the dungeon. This is basic implementation of ranged combat. Currently does not support manual targeting.

--- entity.py
+++ entity.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import copy
-
+import math
 from typing import Tuple, TypeVar, Type, TYPE_CHECKING, Optional, Union
 
 from render_order import RenderOrder
@@ -69,6 +69,12 @@ class Entity:
                     self.gamemap.entities.remove(self)
             self.parent = gamemap
             gamemap.entities.add(self)
+
+    def distance(self, x: int, y: int) -> float:
+        """
+        Return the distance between the current entity and the given (x, y) coordinate.
+        """
+        return math.sqrt((x - self.x) ** 2 + (y - self.y) ** 2)
 
     def move(self, dx: int, dy: int) -> None:
         #Move the entity by the given amount
